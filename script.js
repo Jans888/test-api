78925a0f6483a8c4d44e3ff70188fe628e343e9d
Jans888 <j.sumanas@gmail.com>
2021-09-27 10:54:18 +0300

    Script is updated

--- script.js
+++ script.js
@@ -2,23 +2,21 @@ const fs = require('fs')
 
 const fileContents = fs.readFileSync('log').toString()
 let details = []
-let keyStringStart = '.  AssertionError  '
+let keyStringStart = ' AssertionError '
 let section = fileContents.split(keyStringStart)
 
 if (section.length > 1) {
-    for (var i = 1; i < section.length; i++) {
-        let string = section[i].substring( 0, section[i].indexOf( ", time differense in seconds:" ) ).replace("", "")
-        details.push(string)
-    }
+    let keyStringStart = '#  failure         detail'
+    let keyStringEnd = '#### Finished Execution ####'
+    let sectionTemp = fileContents.substring( 0, fileContents.indexOf(keyStringEnd) ).replace("", "")
+    details = sectionTemp.substring(sectionTemp.indexOf(keyStringStart) + 1).replace(" failure         detail", "")
+
 } else {
     details = ["No issues"]
 }
-
-details = details.join('\n')
-console.log(details);
 
 fs.writeFile("details.txt", details, function (err) {
 
     // Checks if there is an error
     if (err) return console.log(err);
-  });
+})
